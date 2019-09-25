f72b23fab1de0d71d06d46d2f34e6d268a5ddaec
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2019-09-25 09:55:43 +0530

    Created and tested new models

--- core/data_processor.py
+++ core/data_processor.py
@@ -7,9 +7,12 @@ class DataLoader():
 
     def __init__(self, filename, split, cols):
         dataframe = pd.read_csv(filename)
+        #i_split = int(len(dataframe) * split)
+        #self.data_train = dataframe.get(cols).values[:i_split]
+        #self.data_test  = dataframe.get(cols).values[i_split:]
         i_split = int(len(dataframe) * split)
         self.data_train = dataframe.get(cols).values[:i_split]
-        self.data_test  = dataframe.get(cols).values[i_split:]
+        self.data_test  = dataframe.get(cols).values[:i_split]
         self.len_train  = len(self.data_train)
         self.len_test   = len(self.data_test)
         self.len_train_windows = None
