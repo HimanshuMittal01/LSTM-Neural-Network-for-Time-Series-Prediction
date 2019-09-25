e4b88ae7c25e1e0d27c3198504c373f85af32787
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2019-09-25 09:38:41 +0530

    Added some data and changed config file

--- core/data_processor.py
+++ core/data_processor.py
@@ -77,7 +77,11 @@ class DataLoader():
         for window in window_data:
             normalised_window = []
             for col_i in range(window.shape[1]):
-                normalised_col = [((float(p) / float(window[0, col_i])) - 1) for p in window[:, col_i]]
+                if float(window[0, col_i])!=0:
+                    normalised_col = [((float(p) / float(window[0, col_i])) - 1) for p in window[:, col_i]]
+                else:
+                    normalised_col = [((float(p) / float(window[0, col_i]+1)) - 1) for p in window[:, col_i]]
+                
                 normalised_window.append(normalised_col)
             normalised_window = np.array(normalised_window).T # reshape and transpose array back into original multidimensional format
             normalised_data.append(normalised_window)
